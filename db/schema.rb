8321baf6bc0bb89e6d84a14204c1caec033772f1
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2019-12-13 13:24:27 +0100

    add additional field description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_140443) do
+ActiveRecord::Schema.define(version: 2019_12_13_122058) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_140443) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
